51b1eaaf1aa9eb3d16befbeb40e8daf1f9430faf
Yeabsra <tesfayeyeabsra446@gmail.com>
2023-10-11 13:34:28 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next"
+
+export const metadata:Metadata = {
+    title : 'Home',
+    description : "Home Page"
+}
+function Page() {
+  return (
+    <div>Page</div>
+  )
+}
+
+export default Page
